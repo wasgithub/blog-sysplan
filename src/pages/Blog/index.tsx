c7d97893a07742b67e837b65f8056a7a77a871a0
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2021-03-29 13:40:43 -0300

    feat(layout): added structure

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
-import { Container, Box, Typography, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import { ContainerDefault } from "./styles";
 
-import Header from "./components/Header";
-import Body from "./components/Body";
 import Copyright from "../../common/components/Copyright";
 
 import { getPosts } from "../../core/actionCreators/postsActionCreators";
@@ -24,22 +22,20 @@ const Blog: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Blog - AirLiquide
-        </Typography>
-        <Grid item xs={12} data-aos={"fade-up"}>
-          <Header
-            subtitle="Quem tem informação tem poder."
-            disableGutter
-            titleVariant="h3"
-          />
-          <Body />
-        </Grid>
-      </Box>
-      <Copyright />
-    </Container>
+    <ContainerDefault>
+      <header>Header</header>
+      <nav>
+        <div />
+      </nav>
+      <main>
+        <div />
+        <div />
+      </main>
+      <aside>links</aside>
+      <footer>
+        <Copyright />
+      </footer>
+    </ContainerDefault>
   );
 };
 
--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -1,6 +1,52 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  font-size: 48px;
-  color: #3a3a3a;
+export const ContainerDefault = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  @media (min-width: 768px) {
+    & {
+      display: grid;
+      grid-template-columns: minmax(200px, 0.7fr) 1fr minmax(100px, 0.5fr);
+      grid-template-rows: auto 1fr auto;
+    }
+  }
+
+  header {
+    grid-column: span 3;
+    padding: 30px;
+    text-align: center;
+    font-size: 1.4em;
+    background-color: #369;
+    color: white;
+  }
+
+  main {
+    flex: 1;
+    padding: 20px;
+  }
+
+  nav {
+    padding: 20px;
+  }
+
+  aside {
+    padding: 20px;
+    background-color: #936;
+  }
+
+  footer {
+    grid-column: span 3;
+    padding: 30px;
+    text-align: center;
+    font-size: 1.4em;
+    color: white;
+  }
+
+  h1 {
+    margin-bottom: 1em;
+    font-size: 1.3em;
+    font-weight: bold;
+  }
 `;
